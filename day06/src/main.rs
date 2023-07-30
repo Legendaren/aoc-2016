cc8526f416bfd2e70e5ae8f2d607b0cbb910888b
Legendaren <Legendaren@users.noreply.github.com>
2023-07-30 18:26:44 +0200

    day06 part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -18,6 +18,26 @@ fn part1() -> String {
     message
 }
 
+fn part2() -> String {
+    let file_content = fs::read_to_string("input.txt").unwrap();
+    let lines: Vec<&str> = file_content.lines().collect();
+    let mut message = String::new();
+    let column_count = lines[0].len();
+
+    for i in 0..column_count {
+        let chars_in_column: Vec<char> = lines
+            .iter()
+            .map(|line| line.chars().nth(i).unwrap())
+            .collect();
+        let char_counts = chars_in_column.iter().collect::<Counter<_>>();
+        let mut least_common = char_counts.most_common();
+        least_common.reverse();
+        message.push(*least_common.first().unwrap().0);
+    }
+    message
+}
+
 fn main() {
     println!("{}", part1());
+    println!("{}", part2());
 }
