34098b979e139f418fdeb7161c76e98bef7649da
Legendaren <Legendaren@users.noreply.github.com>
2023-08-14 22:46:58 +0200

    day10 cleanup

--- day10/main.ts
+++ day10/main.ts
@@ -1,5 +1,14 @@
 import { readFileSync } from "fs"
 
+function addToMapArray(map: Map<number, number[]>, key, value) {
+    if (map.has(key)) {
+        const arr = map.get(key)
+        arr.push(value)
+    } else {
+        map.set(key, [value])
+    }
+}
+
 function part1() {
     const lines = readFileSync("input.txt", "utf8").toString().split("\n")
     const regexValueGoes = /value (?<value>\d+) goes to bot (?<bot>\d+)/
@@ -14,12 +23,7 @@ function part1() {
     for (const line of lines) {
         const groups = line.match(regexValueGoes)?.groups
         if (groups) {
-            if (botWithChips.has(+groups.bot)) {
-                const arr = botWithChips.get(+groups.bot)
-                arr.push(+groups.value)
-            } else {
-                botWithChips.set(+groups.bot, [+groups.value])
-            }
+            addToMapArray(botWithChips, +groups.bot, +groups.value)
         } else {
             const groups = line.match(regexGivesLowHigh).groups
             fromToLow.set(+groups.senderBot, [
@@ -35,48 +39,36 @@ function part1() {
 
     while (botWithChips.size > 0) {
         for (const [key, arr] of botWithChips.entries()) {
-            if (arr.length >= 2) {
-                const [elem1, elem2] = arr
-                const [low, high] =
-                    elem1 <= elem2 ? [elem1, elem2] : [elem2, elem1]
-
-                if (low === 17 && high === 61) {
-                    console.log(`Part 1 answer: ${key}`)
-                }
-
-                const [lowNum, lowType] = fromToLow.get(key)
-                if (lowType === "bot") {
-                    if (botWithChips.has(lowNum)) {
-                        const arr = botWithChips.get(lowNum)
-                        arr.push(low)
-                    } else {
-                        botWithChips.set(lowNum, [low])
-                    }
-                } else {
-                    outputs.set(lowNum, low)
-                }
-
-                const [highNum, highType] = fromToHigh.get(key)
-                if (highType === "bot") {
-                    if (botWithChips.has(highNum)) {
-                        const arr = botWithChips.get(highNum)
-                        arr.push(high)
-                    } else {
-                        botWithChips.set(highNum, [high])
-                    }
-                } else {
-                    outputs.set(highNum, high)
-                }
-
-                botWithChips.delete(key)
+            if (arr.length < 2) {
+                continue
             }
+
+            const [elem1, elem2] = arr
+            const [low, high] = elem1 <= elem2 ? [elem1, elem2] : [elem2, elem1]
+
+            if (low === 17 && high === 61) {
+                console.log(`Part 1 answer: ${key}`)
+            }
+
+            const forwardValue = (
+                fromTo: Map<number, [number, string]>,
+                key: number,
+                val: number
+            ) => {
+                const [num, type] = fromTo.get(key)
+                if (type === "bot") {
+                    addToMapArray(botWithChips, num, val)
+                } else {
+                    outputs.set(num, val)
+                }
+            }
+
+            forwardValue(fromToLow, key, low)
+            forwardValue(fromToHigh, key, high)
+
+            botWithChips.delete(key)
         }
     }
-
-    console.log(`botWithChips: ${[...botWithChips.entries()]}`)
-    console.log(`fromToLow: ${[...fromToLow.entries()]}`)
-    console.log(`fromToHigh: ${[...fromToHigh.entries()]}`)
-    console.log(`outputs: ${[...outputs.entries()]}`)
 
     const part2Answer = outputs.get(0) * outputs.get(1) * outputs.get(2)
     console.log(`Part 2 answer: ${part2Answer}`)
